67d55c1de8df03024f5dfe7f15d417a1450c59da
j-engel <j-engel@users.noreply.github.com>
2021-09-23 17:08:19 +0200

    Implement JS

--- js/similarWorks.js
+++ js/similarWorks.js
@@ -1,1 +1,19 @@
-console.log ("Hello, i am the KMS JS file and ready for some action :) ");
+$('#similarWorksList').on('mouseover','img',function(e) {
+	var selectedTags = {};
+	$.each($(e.currentTarget).parents('li').data('tag-id-list').split(','),
+		function(i, val) {
+			selectedTags[val]=true; });
+	$('#similarWorksTags li').each( function(i) {
+		var $this = $(this); 
+		if (selectedTags[$this.data('tag-id')]) { 
+			$this.show(); 
+		} else { 
+			$this.hide();
+		} 
+	});
+});
+$('#similarWorksList').on('mouseout','img',function(e) {
+	$('#similarWorksTags li').each( function() { 
+		$(this).show(); 
+	});
+});
